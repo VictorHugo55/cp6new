31183544f587dc7647a356c2e48704163dc56d2a
VictorHugo55 <davichugopereira@gmail.com>
2025-02-26 10:38:55 -0300

    atualizando repositorio do album

--- src/main/java/org/example/Repositories/AlbumRepositorie.java
+++ src/main/java/org/example/Repositories/AlbumRepositorie.java
@@ -1,11 +1,14 @@
 package org.example.Repositories;
 
 import org.example.Entities.Album;
+import org.example.Entities.Artista;
 import org.example.Log.Loggable;
 import org.example.infrastructure.DataBaseConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,16 +27,36 @@ public class AlbumRepositorie implements GenericRepositorie<Album>, Loggable<Alb
 
             stmt.setString(1, album.getTitulo());
             stmt.setInt(2, album.getAnoLancamento());
-            stmt.setInt(3, );
+            stmt.setInt(3, album.getArtista().getId());
 
-
+            stmt.executeUpdate();
+            logInfo("Album registered successfully!");
+        }catch (SQLException e){
+            e.printStackTrace();
+            logInfo("Error registering album");
         }
 
     }
 
     @Override
     public ArrayList<Album> display() {
-        return null;
+        String sql = "SELECT * FROM Artista";
+        ArrayList<Album>albums = new ArrayList<>();
+        try(Connection conn = DataBaseConfig.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()){
+
+            while (rs.next()){
+                Album album = new Album();
+                album.setId(rs.getInt("id_album"));
+                album.setTitulo(rs.getString("titulo_album"));
+                album.setAnoLancamento(rs.getInt("ano_lancamento_album"));
+
+                Artista artista = artistaRepositorie.findById(rs.getInt("artista_id"));
+            }
+
+        }
+        return albums;
     }
 
     @Override
